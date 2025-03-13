2fb534a91388dea923a5efb1dfc47e5a0c3016b0
Msnewbee <bilariko2@gmail.com>
2025-03-13 23:40:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-// Fungsi untuk memuat embed video ke dalam kontainer
-function loadEpisode(embedUrl) {
-  // Buat iframe embed code dengan URL yang diberikan
-  var iframe = '<iframe width="640" height="360" frameborder="0" src="' + embedUrl + '" allowfullscreen></iframe>';
-  // Set konten dari kontainer dengan id "episode-player" menjadi iframe tersebut
-  document.getElementById('episode-player').innerHTML = iframe;
+function loadEpisode(element) {
+    const embedUrl = element.getAttribute("data-url");
+
+    // Cek apakah elemen tersedia
+    if (!embedUrl) {
+        alert("URL episode tidak ditemukan!");
+        return;
+    }
+
+    // Ganti konten video player
+    document.getElementById('episode-player').innerHTML = 
+        `<iframe width="640" height="360" frameborder="0" src="${embedUrl}" allowfullscreen></iframe>`;
 }
