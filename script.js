8e592446ececd6c118abeb1028d6cd3184fe714c
Msnewbee <bilariko2@gmail.com>
2025-03-14 22:13:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -20,20 +20,9 @@ function isValidUrl(url) {
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  const lazyImages = document.querySelectorAll("img.lazy");
-  
-  const imageObserver = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        const img = entry.target;
-        img.src = img.dataset.src;
-        img.classList.remove("lazy");
-        observer.unobserve(img);
-      }
+    let lazyImages = document.querySelectorAll("img.lazy");
+    lazyImages.forEach(img => {
+        img.setAttribute("src", img.getAttribute("data-src"));
+        img.removeAttribute("data-src");
     });
-  });
-  
-  lazyImages.forEach(img => {
-    imageObserver.observe(img);
-  });
 });
