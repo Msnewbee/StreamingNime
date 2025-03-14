075ba4219920ca904a8ff94023831e34889870bf
Msnewbee <bilariko2@gmail.com>
2025-03-14 14:31:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -40,3 +40,31 @@ fetch('https://api.cloudflare.com/client/v4/pages/webhooks/deploy_hooks/dd226011
 .then(data => console.log('Deploy triggered:', data))
 .catch(err => console.error('Error triggering deploy:', err));
 
+async function run(model, input) {
+  const response = await fetch(
+    `https://api.cloudflare.com/client/v4/accounts/b28649ce82eac859f113666c482934e9/ai/run/${model}`,
+    {
+      headers: { Authorization: "Bearer {API_TOKEN}" },
+      method: "POST",
+      body: JSON.stringify(input),
+    }
+  );
+  const result = await response.json();
+  return result;
+}
+
+run("@cf/meta/llama-3-8b-instruct", {
+  messages: [
+    {
+      role: "system",
+      content: "You are a friendly assistan that helps write stories",
+    },
+    {
+      role: "user",
+      content:
+        "Write a short story about a llama that goes on a journey to find an orange cloud ",
+    },
+  ],
+}).then((response) => {
+  console.log(JSON.stringify(response));
+});
